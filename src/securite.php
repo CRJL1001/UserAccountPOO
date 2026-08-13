26810f23e0625300212c1549b64bc572a7c44f14
CRJL1001 <constantlescutprive2@gmail.com>
2026-08-13 13:56:17 +0200

    Delete src directory

--- src/securite.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Securite {
-
-    public static function chiffrer(string $password) : string {
-        $crypted_password = password_hash($password, PASSWORD_DEFAULT); 
-        return $crypted_password;
-    }
-}
